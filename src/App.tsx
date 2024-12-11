898438fc09c70a0d016a4c54cc5c230fb20ebc4d
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2024-12-11 16:49:31 +0900

    [init] create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>コーポレートサイト</h1>
+        <p>すごい企業やねん</p>
+        <ul>
+          <li><a href="https://example.com/">お知らせ</a></li>
+          <li><a href="https://example.com/">サービス</a></li>
+          <li><a href="https://example.com/">お問い合わせ</a></li>
+        </ul>
       </header>
     </div>
   );
