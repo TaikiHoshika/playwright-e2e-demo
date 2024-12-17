f6ce512c4fca265ecc561ea1b594e8757851ba58
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2024-12-17 16:21:39 +0900

    [add] headのタグテストを実装

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,12 @@ import { test, expect } from "@playwright/test";
 
 test('check meta tags', async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await expect(page).toHaveTitle("React App");
 
   await expect(page.locator('meta[charset="utf-8"]')).not.toBeNull();
+  await expect(page.locator('title')).not.toBeNull();
+  await expect(page.locator('meta[property="og:title"]')).not.toBeNull();
+  await expect(page.locator('meta[property="og:description"]')).not.toBeNull();
+  await expect(page.locator('meta[property="og:image"]')).not.toBeNull();
+  await expect(page.locator('meta[property="og:type"]')).not.toBeNull();
+  await expect(page.locator('meta[property="og:url"]')).not.toBeNull();
 });
