fae220bf2605f9a64d820817968f899dae0b9029
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2024-12-18 16:50:51 +0900

    [modify] configを修正

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test('check meta tags', async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
 
   await expect(page.locator('meta[charset="utf-8"]')).not.toBeNull();
   await expect(page.locator('title')).not.toBeNull();
